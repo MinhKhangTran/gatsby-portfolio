52279a4fad4f69ef64f94a1b4a81c987cbd938b7
MinhKhangTran <minhkhang.tran@gmx.de>
2021-05-06 05:16:21 +0200

    add netlify cli and test read-comments query

---
+++ functions/read-comments.js
@@ -0,0 +1,30 @@
+const query = require("./utils/query");
+
+const READ_COMMENTS = `
+query READ_ALL_COMMENTS {
+    allComments {
+      data {
+        _id
+        text
+        user
+        slug
+      }
+    }
+  }
+`;
+
+//using the query function which returns data, errors from axios
+
+exports.handler = async () => {
+  const { data, errors } = await query(READ_COMMENTS);
+  if (errors) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(errors),
+    };
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ messages: data.allComments.data }),
+  };
+};
---
+++ functions/utils/query.js
@@ -0,0 +1,19 @@
+const axios = require("axios");
+require("dotenv").config();
+
+//function which makes a post request to the backend and returns (axios) promise
+
+module.exports = async (query, variables) => {
+  const result = await axios({
+    url: "https://graphql.fauna.com/graphql",
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.FAUNA_SERVER_SECRET}`,
+    },
+    data: {
+      query,
+      variables,
+    },
+  });
+  return result.data;
+};
